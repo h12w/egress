ba9efff7fd81d74a28cfbecab850f6d60c554a86
Hǎiliàng Wáng <hwang.dev@gmail.com>
2015-01-27 13:09:01 +0800

    improve remote error handling

--- remote/serve.go
+++ remote/serve.go
@@ -11,6 +11,7 @@ func Serve(w http.ResponseWriter, r *http.Request) {
 	req, err := protocol.UnmarshalRequest(r.Body)
 	if err != nil {
 		ctx.Errorf("fail to unmarshal a request: %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	defer req.Body.Close()
@@ -19,11 +20,17 @@ func Serve(w http.ResponseWriter, r *http.Request) {
 	resp, err := ctx.NewClient().Transport.RoundTrip(req)
 	if resp == nil {
 		ctx.Errorf("fail to fetch: %v", err)
+		w.WriteHeader(http.StatusGatewayTimeout)
 		return
 	}
 	defer resp.Body.Close()
 	if err := protocol.MarshalResponse(resp, w); err != nil {
 		ctx.Errorf("fail to marshal a response: %s", err.Error())
+		if hij, ok := w.(http.Hijacker); ok {
+			if conn, _, err := hij.Hijack(); err != nil {
+				conn.Close() // force closing
+			}
+		}
 		return
 	}
 }
